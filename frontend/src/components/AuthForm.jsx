e671b7b6e653f47034badb1507e10db5dd9604d6
Akshat-hype <akshatgoel310805@gmail.com>
2025-03-21 01:30:22 +0530

    nothing

--- frontend/src/components/AuthForm.jsx
+++ frontend/src/components/AuthForm.jsx
@@ -5,24 +5,24 @@ import { signInWithGoogle } from "./firebase";
 const AuthForm = () => {
   return (
     // <div className="auth-container">
-      <div className="auth-box">
-        {/* Left Side */}
-        <div className="auth-left">
-          <h2>Welcome Back!</h2>
-          <p>Track your PCOS health with ease</p>
-        </div>
-
-        {/* Right Side */}
-        <div className="auth-right">
-          <h2>Login</h2>
-          <p className="sub-text">Continue with your Google account</p>
-
-          {/* Google Auth Button */}
-          <button className="google-btn" onClick={signInWithGoogle}>
-            Continue with Google
-          </button>
-        </div>
+    <div className="auth-box">
+      {/* Left Side */}
+      <div className="auth-left">
+        <h2>Welcome Back!</h2>
+        <p>Track your PCOS health with ease</p>
       </div>
+
+      {/* Right Side */}
+      <div className="auth-right">
+        <h2>Login</h2>
+        <p className="sub-text">Continue with your Google account</p>
+
+        {/* Google Auth Button */}
+        <button className="google-btn" onClick={signInWithGoogle}>
+          Continue with Google
+        </button>
+      </div>
+    </div>
     // </div>
   );
 };
