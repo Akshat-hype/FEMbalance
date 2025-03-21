5c016dc735b16470ac663b1fb6b4fb2cffa80d2b
divyanshii10 <149924007+divyanshii10@users.noreply.github.com>
2025-03-21 17:00:48 +0530

    edit login page and added icon of webapp

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,8 +4,9 @@ import ImageSlider from "./pages/ImageSlider";
 import Page3 from "./pages/Page1";
 
 function App() {
-  return <Page3/>;
-  return <Page1 />;
+  // return <Page3/>;
+  // return <Page1 />;
+   return <AuthForm />;
 }
 
 export default App;
